dbbdae620b1907cad239340c81262199a01d0d11
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-20 14:45:59 +0500

    Connecting MYSql with Node

--- sql/index.js
+++ sql/index.js
@@ -1,14 +1,29 @@
 const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "app",
+  password: "root",
+});
+
+try {
+  connection.query("Show Tables", (err, result) => {
+    if (err) throw new Error();
+    console.log(result);
+  });
+} catch (error) {
+  console.log(error);
+}
+connection.end();
 
 let createRandomUser = () => {
   return {
     userId: faker.string.uuid(),
     username: faker.internet.userName(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
 };
 
