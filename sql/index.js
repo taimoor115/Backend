9736b8096ec664b8ff084730810f5348b608bea2
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-20 19:09:41 +0500

    EDIT Username

--- sql/index.js
+++ sql/index.js
@@ -1,13 +1,16 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 const path = require("path");
+const methodOverride = require("method-override");
 const express = require("express");
 const app = express();
 let port = 8080;
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join((__dirname, "public"))));
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -29,7 +32,7 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/users", (req, res) => {
+app.get("/user", (req, res) => {
   let q = "SELECT * FROM user";
   try {
     connection.query(q, (error, result) => {
@@ -40,6 +43,24 @@ app.get("/users", (req, res) => {
   } catch (error) {
     res.send("Error in DB");
   }
+});
+
+app.get("/user/:id/edit", (req, res) => {
+  const { id } = req.params;
+  let q = `SELECT * FROM user WHERE id ="${id}"`;
+  try {
+    connection.query(q, (error, result) => {
+      if (error) throw error;
+      console.log(result);
+      let user = result[0];
+      res.render("edit", { user });
+    });
+  } catch (error) {
+    res.send("Something Happen in DB");
+  }
+});
+app.patch("/user/:id", (req, res) => {
+  res.send("Success");
 });
 
 app.listen(port, () => {
