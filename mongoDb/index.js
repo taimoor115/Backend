45c01a05ceb403322529379215459f9bcd1fd5fe
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-23 16:32:17 +0500

    Schema for Mongo

--- mongoDb/index.js
+++ mongoDb/index.js
@@ -7,3 +7,9 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
