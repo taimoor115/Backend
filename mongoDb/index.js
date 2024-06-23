7fe9d75ea4f83cb142fd1453e97da5af7696c0b4
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-23 16:19:02 +0500

    Connecting Mongoose with Javascript

---
+++ mongoDb/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+main()
+  .then(() => console.log("Conntection"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
