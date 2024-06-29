7bd8e2f64fc00fc04e8e7212a3bca15ac8ab0a40
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-06-30 00:53:34 +0500

    connect-flash

--- express/index.js
+++ express/index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
-
+const flash = require("connect-flash");
+const path = require("path");
 const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const sesstionOptions = {
   secret: "mysecretkey",
   resave: false,
   saveUninitialized: true,
 };
+
 app.use(session(sesstionOptions));
+app.use(flash());
 
 app.get("/register", (req, res) => {
   let { name = "Anonymous" } = req.query;
   req.session.name = name;
-  res.send(`User ${name}`);
+  req.flash("success", "User registrations successfully");
+  res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-  res.send(`Hello ${req.session.name}`);
+  res.render("page", { name: req.session.name, message: req.flash("success") });
 });
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
